18bad2d77831dcd842f99df8fc0be8e2842c495a
-veldrane <gveldrane@gmail.com>
2025-08-29 11:47:52 +0200

    fix(workers): added more diskspace to work nodes

--- deploy/06-k8s/build_cluster.sh
+++ deploy/06-k8s/build_cluster.sh
@@ -7,13 +7,14 @@ cd ansible/
 echo "Building vms for student $i"
 echo "  - Building master"
 export suffix=$(head -c 16 /dev/urandom | sha1sum | cut -c1-6)
-ansible-playbook 00_prepare_setup.yaml -e "student=2" -e "k8s_role=master" -e "node_index=1" -e "suffix=$suffix"
+ansible-playbook 00_prepare_setup.yaml -e "student=4" -e "k8s_role=master" -e "node_index=1" -e "suffix=$suffix"
 ansible-playbook 01_prepare_nodes.yaml
 ansible-playbook 02_configure_master.yaml
 ansible-playbook 03_deploy_master.yaml
+ansible-playbook 11_register_svc.yaml
 for m in $(seq 1 $WORKERS); do
   echo "  - Building worker $m"
-  ansible-playbook 00_prepare_setup.yaml -e "student=2" -e "k8s_role=worker" -e "node_index=$m" -e "suffix=$suffix"
+  ansible-playbook 00_prepare_setup.yaml -e "student=4" -e "k8s_role=worker" -e "node_index=$m" -e "suffix=$suffix"
   ansible-playbook 01_prepare_nodes.yaml
   ansible-playbook 04_configure_worker.yaml
   ansible-playbook 05_deploy_worker.yaml
